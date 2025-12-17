bba25216e8e2cfcab4289f48482fa61ffb201a6d
Nathanael Smith <nathanael.smith@hilton.com>
2025-12-17 12:23:19 -0600

    duration calculation

--- plugin.ts
+++ plugin.ts
@@ -6,10 +6,15 @@ export const postTasksExecution: PostTasksExecution = async (
 ) => {
   console.log(context.taskResults);
   for (const [project, result] of Object.entries(context.taskResults)) {
+    const start = new Date(result.startTime).toISOString();
+    const end = new Date(result.endTime).toISOString();
     console.log({
       project,
       status: result.status,
       terminalOutput: result.terminalOutput,
+      start,
+      end,
+      durationMs: result.endTime - result.startTime,
       startTime: result.startTime,
       endTime: result.endTime,
     });
