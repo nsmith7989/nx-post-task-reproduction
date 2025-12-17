512f3ca1b2bbca479d9d5e5f7d19f6f63f010734
Nathanael Smith <nathanael.smith@hilton.com>
2025-12-17 12:47:44 -0600

    minimal repro example

--- plugin.ts
+++ plugin.ts
@@ -5,18 +5,4 @@ export const postTasksExecution: PostTasksExecution = async (
   context
 ) => {
   console.log(context.taskResults);
-  for (const [project, result] of Object.entries(context.taskResults)) {
-    const start = new Date(result.startTime).toISOString();
-    const end = new Date(result.endTime).toISOString();
-    console.log({
-      project,
-      status: result.status,
-      terminalOutput: result.terminalOutput,
-      start,
-      end,
-      durationMs: result.endTime - result.startTime,
-      startTime: result.startTime,
-      endTime: result.endTime,
-    });
-  }
 };
