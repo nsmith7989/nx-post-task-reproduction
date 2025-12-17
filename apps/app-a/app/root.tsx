78eb3c8730c03f15d289137cd66b92d26746485e
Nathanael Smith <nathanael.smith@hilton.com>
2025-12-17 16:01:51 -0600

    add non dte mode

--- apps/app-a/app/root.tsx
+++ apps/app-a/app/root.tsx
@@ -39,7 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <h1>Hello world</h1>
+        <h1>Hello world!</h1>
         <AppNav />
         {children}
         <ScrollRestoration />
